0a688355baa6761ecc6b3b2a065d6cfdb44aeafc
Odysseas Lamtzidis <odyslam@gmail.com>
2022-03-15 08:31:53 +0200

    chore: change dirs, add proxyAdmin interface

--- src/utils/upgradeProxy.sol
+++ src/utils/upgradeProxy.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 
 import {DeployProxy} from "./deployProxy.sol";
-import {ProxyAdmin} from "openzeppelin/proxy/transparent/ProxyAdmin.sol";
+import {ProxyAdminInterface} from "./IProxyAdmin.sol";
 
 
 contract UpgradeProxy is DeployProxy {
@@ -14,7 +14,7 @@ contract UpgradeProxy is DeployProxy {
         // Load the expectEmit cheatcode for the Upgraded event. Since it's only a single argument, we only need
         // the first flag set to True.
         vm.expectEmit(true, false, false, false);
-        emit Upgraded(newImplementation)
+        emit Upgraded(newImplementation);
         // Check if the Admin address is a smart contract Admin or an EOA.
         // If it's an admin, the Upgrade will need to pass through the Admin, called by the owner EOA of the admin smart
         // contract.
@@ -22,7 +22,7 @@ contract UpgradeProxy is DeployProxy {
         // vm.prank tells the Foundry VM to make the call from that particular address.
         if (isContract(admin)){
             vm.prank(owner);
-            ProxyAdmin(admin).upgrade(uupsProxy, newImplementation);
+            ProxyAdminInterface(admin).upgrade(uupsProxy, newImplementation);
         }
         else {
             vm.prank(admin);
@@ -33,14 +33,15 @@ contract UpgradeProxy is DeployProxy {
     /// @notice Upgrade a proxy smart contract and also pass calldata to be called with the update.
     function upgradeProxy(proxyType proxy, address newImplementation, bytes memory data, address admin, address owner) public {
         vm.expectEmit(true, false, false, false);
-        emit Upgraded(newImplementation)
+        emit Upgraded(newImplementation);
         if (isContract(admin)){
             vm.prank(owner);
-            ProxyAdmin(admin).upgradeAndCall(uupsProxy, newImplementation, data);
+            ProxyAdminInterface(admin).upgradeAndCall(uupsProxy, newImplementation, data);
         }
         else {
             vm.prank(admin);
             uupsProxy.upgradeToAndCall(newImplementation, data);
+        }
     }
 
     function isContract(address addr) public view returns (bool) {
